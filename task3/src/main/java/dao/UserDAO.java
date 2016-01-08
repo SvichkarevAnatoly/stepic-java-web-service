b5fb4463be7fb4cac0e44db798e67b8f7a607a86
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-01-08 19:43:38 +0600

    create DAOs

---
+++ task3/src/main/java/dao/UserDAO.java
@@ -0,0 +1,24 @@
+package dao;
+
+import datasets.UserDataSet;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+public class UserDAO {
+    private Session session;
+
+    public UserDAO(Session session) {
+        this.session = session;
+    }
+
+    public UserDataSet getUserDataSetByLogin(String login) throws HibernateException {
+        Criteria criteria = session.createCriteria(UserDataSet.class);
+        return (UserDataSet) criteria.add(Restrictions.eq("login", login)).uniqueResult();
+    }
+
+    public long insertUser(UserDataSet user) throws HibernateException {
+        return (Long) session.save(user);
+    }
+}
