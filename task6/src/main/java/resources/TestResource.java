73f05fc99bdfe83ce50430ab759adf5bdfdfbbed
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-03-05 01:53:01 +0300

    add class from description task6 TestResource

---
+++ task6/src/main/java/resources/TestResource.java
@@ -0,0 +1,24 @@
+package resources;
+
+public class TestResource {
+    private String name;
+    private int age;
+
+    public TestResource(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public TestResource() {
+        this.name = "";
+        this.age = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
