cc86e30d2c8afe6532dedb6c39083c74c65a99b6
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-03-19 00:37:23 +0300

    task7 copy server sample from http://javatutor.net/books/tiej/socket

--- task7/src/main/java/Main.java
+++ task7/src/main/java/Main.java
@@ -1,5 +1,36 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static final int PORT = 5050;
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket s = new ServerSocket(PORT);
+        System.out.println("Started: " + s);
+        try {
+            Socket socket = s.accept();
+            try {
+                System.out.println("Connection accepted: " + socket);
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        socket.getInputStream()));
+                PrintWriter out = new PrintWriter(new BufferedWriter(
+                        new OutputStreamWriter(socket.getOutputStream())), true);
+                while (true) {
+                    String str = in.readLine();
+                    if (str.equals("END"))
+                        break;
+                    System.out.println("Echoing: " + str);
+                    out.println(str);
+                }
+            }
+            finally {
+                System.out.println("closing...");
+                socket.close();
+            }
+        }
+        finally {
+            s.close();
+        }
     }
 }
