d0244ade60823e2ac1acef6263cf153104bca6ea
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-03-19 01:40:57 +0300

    task7 done

--- task7/src/main/java/Main.java
+++ task7/src/main/java/Main.java
@@ -1,31 +1,17 @@
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.io.*;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
     public static final int PORT = 5050;
-    static final Logger logger = LogManager.getLogger(Main.class.getName());
 
     public static void main(String[] args) throws IOException {
+        System.out.println("Server Started");
         try (ServerSocket s = new ServerSocket(PORT)) {
-            System.out.println("Started: " + s);
-            try (Socket socket = s.accept()) {
-                System.out.println("Connection accepted: " + socket);
-                BufferedReader in = new BufferedReader(new InputStreamReader(
-                        socket.getInputStream()));
-                PrintWriter out = new PrintWriter(new BufferedWriter(
-                        new OutputStreamWriter(socket.getOutputStream())), true);
-                while (true) {
-                    String str = in.readLine();
-                    if (str.equals("END"))
-                        break;
-                    System.out.println("Echoing: " + str);
-                    out.println(str);
-                    logger.info(str);
-                }
+            while (true) {
+                final Socket socket = s.accept();
+                final Thread repeatServerThread = new Thread(new RepeatServer(socket));
+                repeatServerThread.start();
             }
         }
     }
---
+++ task7/src/main/java/RepeatServer.java
@@ -0,0 +1,48 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.*;
+import java.net.Socket;
+
+public class RepeatServer implements Runnable {
+    private final Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    public RepeatServer(Socket socket) throws IOException {
+        this.socket = socket;
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(
+                new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+    }
+
+    @Override
+    public void run() {
+        final Logger logger = LogManager.getLogger(
+                RepeatServer.class.getName()
+                + ":thread-"
+                + Thread.currentThread().getId());
+
+        try {
+            while (true) {
+                String str = in.readLine();
+                if (str.equals("Bue.")) {
+                    socket.close();
+                    logger.info("Connection close");
+                    break;
+                } else {
+                    logger.info(str);
+                    out.println(str);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("IO Exception");
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                System.err.println("Socket not closed");
+            }
+        }
+    }
+}
